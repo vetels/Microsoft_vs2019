842f1def7a988db3ac3145ad5449bf5764d93449
Филиппов Виталий Геннадьевич <Filippov_VG@ach.gov.local>
2022-10-11 15:49:01 +0300

    Сделал новый проект, этот просто синхронизирую

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,22 +16,32 @@ public:
         SetConsoleOutputCP(cp);
     }
 
-    // поскольку мы изменили свойства внешнего объекта — консоли, нам нужно
-    // вернуть всё как было (если программа вылетит, пользователю не повезло)
+    // поскольку мы изменили свойства внешнего объекта — консоли, нам нужно 
+    // вернуть всё как было (если программа вылетит, пользователю не повезло) 
     ~ConsoleCP()
     {
         SetConsoleCP(oldin);
         SetConsoleOutputCP(oldout);
     }
 };
-
 // и в программе:    
 int main(int argc, char* argv[])
 {
     ConsoleCP cp(1251);
 // начинай писать код:
     
-   int str = 115
+    const int ROWS = 3;
+    const int COLUMNS = 3;
+    
+    cout << "Это игра крестики нолики:\n";
+    for (int i = 0; i < ROWS; ++i)
+    {
+        for (int j = 0; j < COLUMNS; ++i)
+        {
+            cout << i << "." << j << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
